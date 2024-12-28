73a9ed18e9e0e0b1210077c717c1278aec65e598
cynrio <faisal@cynrio.com>
2024-12-28 11:58:31 +0500

    question 1 solved

---
+++ src/main/java/com/maybank/ArrArgs.java
@@ -0,0 +1,16 @@
+package com.maybank;
+
+public class ArrArgs {
+    public static void main(String[] args) {
+        // args validation if they are present of not
+        if (args.length == 0) {
+            System.out.println("No arguments provided to the program.");
+            return;
+        }
+        // replace do-while with for loop because iterations are limited.
+        // No need to check ArrayIndexOutOfBoundsException
+        for (int k = 0; k < args.length; k++) {
+            System.out.println("Value of input argument " + k + " is: " + args[k]);
+        }
+    }
+}
